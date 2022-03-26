b159b051d0e689edfae98bfcb4d9f0d63b89ff00
Ryan Bloom <ryan@ryan-bloom.com>
2022-03-26 19:00:14 -0400

    Allow filtering votes to one legislator ID

--- server.py
+++ server.py
@@ -4,7 +4,15 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/bills")
-async def bills():
+async def bills(legislator_id=None):
     with open('popular_bills.json') as f:
         results = json.load(f)
+        if legislator_id is not None:
+            for b in results:
+                votes = b["votes"]
+                for v in votes:
+                    if v['id'] == legislator_id:
+                        b['vote'] = v
+                        break
+                del b['votes']
         return {"bills": results}
