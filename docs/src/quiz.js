d59846457c2a89180a9d6f2a75d6cc00927753a2
Jonathan Hail <jjh3wqt@gmail.com>
2022-03-27 03:34:12 -0400

    sketchy globals

--- docs/src/quiz.js
+++ docs/src/quiz.js
@@ -7,6 +7,7 @@ import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import { motion } from 'framer-motion';
 import { Navigate } from 'react-router-dom';
+import './State.js'
 
 export class QuizPage extends React.Component {
 
@@ -28,7 +29,8 @@ export class QuizPage extends React.Component {
             },
             qtype: 0,
             answer: "",
-            response: ""
+            response: "",
+            correct: 0
         }
         this.onKeyPress = this.onKeyPress.bind(this);
         window.fetch("http://127.0.0.1:8000/legislator?lid="+window.location.hash.split("?")[1]).then(res => res.text()).then(data => {this.state.legislator.name = data.slice(1, -1);})
@@ -161,6 +163,7 @@ export class QuizPage extends React.Component {
                     console.log(this.state.legislator.id)
                     if ((answer === "yea" && json === "Yea") || (answer === "nay" && json === "Nay")) {
                         this.setState({answer: "correct"})
+                        this.setState({correct: this.state.correct + 1})
                     } else {
                         this.setState({answer: "incorrect"})
                     }
@@ -168,8 +171,9 @@ export class QuizPage extends React.Component {
             setTimeout(() => {
                 this.setState({answer: "", response: ""});
                 const index = this.state.index + 1
-                if (index >= this.state.bills.length) {
+                if (index >= this.state.bills.length - 1) {
                     console.log('All done')
+                    global.correct = this.state.correct;
                     this.setState({redirect: true})
                 }
                 this.setState({
