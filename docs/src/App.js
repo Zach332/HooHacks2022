99982c5362ecaa24685816d620c4d9a3e5780973
Zachary <heidelzachary@gmail.com>
2022-03-26 20:49:15 -0400

    Add state search

--- docs/src/App.js
+++ docs/src/App.js
@@ -1,5 +1,7 @@
 import Box from '@mui/material/Box';
 import logo from './logo512.png';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
 import './App.css';
 
 function App() {
@@ -13,8 +15,83 @@ function App() {
         >
             <img src={logo} className="large-logo" alt="logo" />
         </Box>
+        <Box
+            sx={{
+            display: 'flex',
+            justifyContent: 'space-evenly',
+            }}
+        >
+            <Autocomplete
+                disablePortal
+                options={states}
+                sx={{ width: 300 }}
+                renderInput={(params) => <TextField {...params} label="State" />}
+            />
+        </Box>
   </Box>
   );
 }
+
+const states = [
+    "Alabama",
+    "Alaska",
+    "American Samoa",
+    "Arizona",
+    "Arkansas",
+    "California",
+    "Colorado",
+    "Connecticut",
+    "Delaware",
+    "District Of Columbia",
+    "Federated States Of Micronesia",
+    "Florida",
+    "Georgia",
+    "Guam",
+    "Hawaii",
+    "Idaho",
+    "Illinois",
+    "Indiana",
+    "Iowa",
+    "Kansas",
+    "Kentucky",
+    "Louisiana",
+    "Maine",
+    "Marshall Islands",
+    "Maryland",
+    "Massachusetts",
+    "Michigan",
+    "Minnesota",
+    "Mississippi",
+    "Missouri",
+    "Montana",
+    "Nebraska",
+    "Nevada",
+    "New Hampshire",
+    "New Jersey",
+    "New Mexico",
+    "New York",
+    "North Carolina",
+    "North Dakota",
+    "Northern Mariana Islands",
+    "Ohio",
+    "Oklahoma",
+    "Oregon",
+    "Palau",
+    "Pennsylvania",
+    "Puerto Rico",
+    "Rhode Island",
+    "South Carolina",
+    "South Dakota",
+    "Tennessee",
+    "Texas",
+    "Utah",
+    "Vermont",
+    "Virgin Islands",
+    "Virginia",
+    "Washington",
+    "West Virginia",
+    "Wisconsin",
+    "Wyoming"
+]
 
 export default App;
