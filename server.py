45d5efa5206edb7b427d7b74c453b915868c22fd
Jonathan Hail <jjh3wqt@gmail.com>
2022-03-26 21:48:44 -0400

    fix people borking json

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
 import json
 from fastapi import FastAPI
 import random
-from summary import get_summary
 
 app = FastAPI()
 
@@ -24,13 +23,7 @@ async def get_legislators(state: str):
     legislators = {}
     with open('popular_bills.json') as f:
         j = json.load(f)
-        for x in j[0]["votes"]["Yea"]:
-            if x["state"] == state:
-                legislators[x["id"]] = x["display_name"]
-        for x in j[0]["votes"]["Nay"]:
-            if x["state"] == state:
-                legislators[x["id"]] = x["display_name"]
-        for x in j[0]["votes"]["Not Voting"]:
+        for x in j[0]["votes"]:
             if x["state"] == state:
                 legislators[x["id"]] = x["display_name"]
     return str(legislators)
@@ -41,20 +34,13 @@ async def get_bill_by_id(lid):
     with open('popular_bills.json') as f:
         j = json.load(f)
         for b in j:
-            c = False
-            for x in b["votes"]["Yea"]:
+            for x in b["votes"]:
                 if x["id"] == lid:
                     bills.append(b["bill"])
                     bills[-1]["title"] = b["subject"]
+                    bills[-1]["summary"] = b["bill_summary"]
                     c = True
                     break
-            if c: continue
-            for x in b["votes"]["Nay"]:
-                if x["id"] == lid:
-                    bills.append(b["bill"])
-                    break
-    for b in bills:
-        b["summary"] = get_summary(b["congress"], "house" if b["type"] == "hr" else "senate", b["number"])
     toreturn = random.sample(bills, 10)
     return str(toreturn)
 
@@ -64,10 +50,9 @@ async def get_answer_by_id(lid, congress: int, number: int, type):
         j = json.load(f)
         for b in j:
             if b["bill"]["number"] == number and b["bill"]["congress"] == congress and b["bill"]["type"] == type:
-                for x in b["votes"]["Yea"]:
-                    if x["id"] == lid:
+                for x in b["votes"]:
+                    if x["id"] == lid and x["vote"] == "Yea":
                         return "Yea"
-                for x in b["votes"]["Nay"]:
-                    if x["id"] == lid:
+                    else:
                         return "Nay"
     return "error"
