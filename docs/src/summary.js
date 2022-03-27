9795e9a6a4aa1e6c8dbcd297450caff42f6dd7f8
Zachary <heidelzachary@gmail.com>
2022-03-27 03:48:28 -0400

    Add confetti

--- docs/src/summary.js
+++ docs/src/summary.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import NavBar from './navbar';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Divider from '@mui/material/Divider';
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import './State.js'
+import Confetti from 'react-confetti'
 
 export class SummaryPage extends React.Component {
     constructor(props) {
@@ -15,6 +11,7 @@ export class SummaryPage extends React.Component {
     render() {
         return <div>
             <NavBar />
+            <Confetti />
             <h1 style={{textAlign: "center", paddingTop: 5, textDecoration: "bold"}}>You scored {global.correct}/10</h1> 
         </div>
     }
